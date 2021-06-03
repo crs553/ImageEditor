802ce9b5d2bec616eb433fa4a7b27aecf8b98059
stubbs774 <charlie.stubbs@ntlworld.com>
2021-06-03 17:54:11 +0100

    Updates to make UI more user friendly

--- Backend.py
+++ Backend.py
@@ -1,53 +1,80 @@
-import PIL.JpegImagePlugin
-import numpy as np
-import PIL as p
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
 from typing import Optional
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfilename
+import numpy as np
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 class ImgOperations:
     def __init__(self) -> None:
-        self.imgPath = self.getImgLink()
-        self.image = self.openImage("/home/charles/Documents/Wallpaper/2106257.png")
-        self.original = self.image
+        """
+        Gets image from an askfilename dialogue
+        """
+        self.stack = []
+        self.imgPath = None
+        self.image = None
+        self.original = None
+        self.raw = None
+
+    """Image Opening and Changing"""
 
     def getImgLink(self) -> Optional[str]:
-        Tk().withdraw()
+        # Tk().withdraw()
         filename = askopenfilename()
         return filename
 
-    def openImage(filename: str):
-        return ImageTk.PhotoImage(Image.open(filename))
+    def setImage(self, filename: str):
+        self.raw = Image.open(filename)
+        self.image = ImageTk.PhotoImage(self.raw)
+        self.setPath(filename)
+        self.setOriginal()
+        self.__addStack(self.image)
 
+    def __addStack(self, val) -> None:
+        self.stack.append(val)
 
-    def getRed(im_data):
-        imRed = np.zeros(im_data.shape, dtype='unit8')
-        imRed[:, :, 0] = im_data[:, :, 0]
-        return Image.fromarray(imRed)
+    def resetStack(self):
+        self.stack = []
 
+    def popStack(self):
+        return self.stack.pop()
 
-    def getGreen(im_data):
-        imGreen = np.zeros(im_data, dtype='uint8')
-        imGreen = im_data[:, :, 1]
-        return Image.fromarray(imGreen)
-
-    def getBlue(im_data):
-        imBlue = np.zeros(im_data, dtype='uint8')
-        imBlue = im_data[:, :, 2]
-        return Image.fromarray(imBlue)
+    def getPath(self):
+        return self.imgPath
 
     def getImage(self):
         return self.image
 
-    def setImage(self, image):
-        self.image = image
+    def setPath(self, filename: str):
+        self.imgPath = filename
+
+    def setOriginal(self):
+        self.original = self.stack[0]
 
     def getOriginal(self):
         return self.original
 
+    def reverse(self):
+        pass
+
     def reverseToOrigin(self):
         self.image = self.original
+
+    """Image Manipulations"""
+
+    def getRed(self,im_data):
+        imRed = np.zeros(im_data.shape, dtype='unit8')
+        imRed[:, :, 0] = im_data[:, :, 0]
+        self.image = Image.fromarray(imRed)
+        self.__addStack(self.image)
+
+    def getGreen(self,im_data):
+        imGreen = np.zeros(im_data, dtype='uint8')
+        imGreen = im_data[:, :, 1]
+        self.image = Image.fromarray(imGreen)
+        self.__addStack(self.image)
+
+    def getBlue(self,im_data):
+        imBlue = np.zeros(im_data, dtype='uint8')
+        imBlue = im_data[:, :, 2]
+        self.image =  Image.fromarray(imBlue)
+        self.__addStack(self.image)
--- Frontend.py
+++ Frontend.py
@@ -1,32 +1,63 @@
-import tkinter
 from tkinter import *
-import Backend
-from tkinter.filedialog import askopenfile
-from functools import partial
-
-root = Tk()
-root.title("Open Image")
-root.geometry("200x300")
-
-back_Img = None
-top = None
-photo = None
+from Backend import ImgOperations
 
 
+class MainApp(Frame):
+    def __init__(self, master=None) -> None:
+        Frame.__init__(self, master)
 
-def openImg():
-    global back_Img
-    back_Img = Backend.ImgOperations()
-    top = Toplevel()
-    btn = Button(top, text="Hello")
-    top.mainloop()
+        self.master = master
+        self.img = ImgOperations()
+        self.imgLab = Label(self, text="Hello")
+        self.imgLab.place(x=0,y=0)
 
-open_btn = Button(root, text="Open Image", command=lambda: openImg())
-open_btn.grid(row=0, column=0)
+        self.init_window()
+
+    def init_window(self) -> None:
+        self.master.title("GUI")
+
+        self.pack(fill=BOTH, expand=1)
+
+        # menu_bar = MenuBar(self.master)
+        # self.master.config(menu=menu_bar)
+        menu = Menu(self.master, tearoff=0)  # , tearoff=False)
+        self.master.config(menu=menu)
+
+        """File Menu Tab"""
+        file = Menu(menu)
+        file.add_command(label="Exit", underline=1, command=self.clse)
+        file.add_command(label='Open', underline=1, command=self.open_File)
+        menu.add_cascade(label="File", underline=0, menu=file)
+
+        """Edit Menu Tab"""
+        edit = Menu(menu)
+        edit.add_command(label="Undo", underline=1, command=self.img.reverse)
 
 
+    def open_File(self):
+        filename = self.img.getImgLink()
+        self.img.setImage(filename)
+        self.imgLab.destroy()
+        self.imgLab = Label(self, image=self.img.getImage())
+        self.imgLab.image = self.img.getImage()
+        self.imgLab.place(x=0, y=0)
 
-root.mainloop()
+    def clse(self) -> None:
+        exit()
 
-# if __name__ == "__main__":
-#     main()
+    def undo(self) -> None:
+        """
+        Function for reversing image changes
+        """
+        pass
+
+
+def main():
+    root = Tk()
+    root.geometry("900x900")
+    app = MainApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
